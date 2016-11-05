94ec061c17fb1e69fe193baaaa5838e6360b76f3
Hoa Tran <minhhoa.work@gmail.com>
2016-11-05 22:11:18 +0700

    physics funding data-vis

---
+++ js/index3.js
@@ -0,0 +1,363 @@
+﻿var container = d3.select("#divContainer");
+var $container = $("#divContainer");
+
+var data, chartData;
+
+var dictClass = [], dictOrg = [], dictOrg_type = [], dictPclass = [], dictStatus = [], dictOwner = [], dictGrant = [], dictScheme = [];
+var startFrom = 1000000, startTo = 0, endFrom = 1000000, endTo = 0;
+
+function scanValue() {
+    
+    var timeParse = d3.timeParse("%m/%d/%Y");
+
+    data.forEach(function (d) {
+        if (dictClass.indexOf(d.class) < 0) dictClass.push(d.class);
+        if (dictOrg.indexOf(d.org) < 0) dictOrg.push(d.org);
+        if (dictOrg_type.indexOf(d.org_type) < 0) dictOrg_type.push(d.org_type);
+        if (dictPclass.indexOf(d.pclass) < 0) dictPclass.push(d.pclass);
+        if (dictStatus.indexOf(d.status) < 0) dictStatus.push(d.status);
+        if (dictOwner.indexOf(d.owner) < 0) dictOwner.push(d.owner);
+        if (dictGrant.indexOf(d.grant) < 0) dictGrant.push(d.grant);
+        if (dictScheme.indexOf(d.scheme) < 0) dictScheme.push(d.scheme);
+
+        d.startY = timeParse(d.start).getFullYear();
+        d.endY = timeParse(d.end).getFullYear();
+
+        if (d.startY < startFrom) startFrom = d.startY;
+        if (d.startY > startTo) startTo = d.startY;
+        
+        if (d.endY < endFrom) endFrom = d.endY;
+        if (d.endY > endTo) endTo = d.endY;
+    });
+
+    dictClass.sort();
+    dictOrg.sort();
+    dictOrg_type.sort();
+    dictPclass.sort();
+    dictStatus.sort();
+    dictOwner.sort();
+    dictGrant.sort();
+    dictScheme.sort();
+
+    //var dict = dictOrg;
+    
+    //var html = "count: " + dict.length + "<br/><br/>";
+    //dict.forEach(function (d) {
+    //    html += d + "<br/>";
+    //});
+    
+    //html += "<br/>start: " + startFrom + " - " + startTo;
+    //html += "<br/>end: " + endFrom + " - " + endTo;
+
+    //$container.html(html);
+
+    initFilter(dictOwner, $("#divFilters ul[data-id='owner']"));
+    initFilter(dictPclass, $("#divFilters ul[data-id='pclass']"));
+    initFilter(dictOrg_type, $("#divFilters ul[data-id='org_type']"));
+    initFilter(dictStatus, $("#divFilters ul[data-id='status']"));
+    initFilter(dictGrant, $("#divFilters ul[data-id='grant']"));
+}
+
+function initFilter(dict, ul) {
+    var html = "";
+
+    dict.forEach(function(d) {
+        html += "<li class='active'><span>" + (d == null || d == "" ? "[empty]" : d) + "</span><a>[only]</a></li>";
+    });
+
+    ul.html(html);
+}
+
+function refreshChart() {
+    container.selectAll("*").remove();
+    
+    var mode = $("#divDisplayModes .btn.active").attr("data-id");
+    
+    if (mode == "trend") {
+        drawLines();
+        return;
+    }
+
+    var items = [], valueDict = [], recordDict = [];
+    var recordMin = 1000000, recordMax = 0;
+
+    var itemDict = mode == "pclass" ? dictPclass : mode == "class" ? dictClass : mode == "org" ? dictOrg : dictScheme;
+
+    itemDict.forEach(function (d) {
+        valueDict[d] = 0;
+        recordDict[d] = 0;
+    });
+
+    if (mode == "pclass") {
+        chartData.forEach(function (d) {
+            valueDict[d.pclass] += parseFloat(d.total);
+            recordDict[d.pclass]++;
+        });
+        
+    } else if (mode == "class") {
+        chartData.forEach(function (d) {
+            valueDict[d.class] += parseFloat(d.total);
+            recordDict[d.class]++;
+        });
+        
+    } else if (mode == "org") {
+        chartData.forEach(function (d) {
+            valueDict[d.org] += parseFloat(d.total);
+            recordDict[d.org]++;
+        });
+        
+    } else {
+        chartData.forEach(function (d) {
+            valueDict[d.scheme] += parseFloat(d.total);
+            recordDict[d.scheme]++;
+        });
+    }
+
+    $.each(itemDict, function (i, d) {
+        var records = recordDict[d];
+        if (records == 0) return;
+
+        items.push({ id: i, name: (d == null || d == "" ? "[empty]" : d), value: valueDict[d], records: records });
+
+        if (records < recordMin) recordMin = records;
+        if (records > recordMax) recordMax = records;
+    });
+
+    $("#divDisplayModes > div").show();
+    if (items.length == 0) return;
+    
+    $("#spanRecords").html(recordMin + " - " + recordMax);
+
+    var r = Math.min($container.width(), $container.height());
+
+    var color = d3.scaleLinear().domain([Math.sqrt(recordMin), Math.sqrt(recordMax)]).interpolate(d3.interpolateHslLong).range(['#007DB4', '#FFA600']);
+
+    var pack = d3.pack().size([r, r]).padding(1);
+
+    var root = d3.hierarchy({ children: items }).sum(function(d) { return d.value; });
+
+    var svg = container.append("svg").attr("width", r).attr("height", r).attr("class", "bubble");
+
+    var node = svg.selectAll(".node")
+        .data(pack(root).leaves()).enter().append("g").attr("class", "node").attr("opacity", 0)
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+    node.append("circle")
+        .attr("id", function(d) { return "node" + d.data.id; })
+        .attr("r", function(d) { return d.r; })
+        .style("fill", function (d) { return color(Math.sqrt(d.data.records)); });
+
+    node.append("clipPath")
+        .attr("id", function(d) { return "clip" + d.data.id; })
+        .append("use")
+        .attr("xlink:href", function(d) { return "#node" + d.data.id; });
+
+    node.append("text")
+        .attr("clip-path", function(d) { return "url(#clip" + d.data.id + ")"; })
+        .selectAll("tspan")
+        .data(function(d) { return d.data.name.split(/(?=[A-Z][^A-Z])/g); })
+        .enter().append("tspan")
+        .attr("x", 0)
+        .attr("y", function(d, i, nodes) { return 13 + (i - nodes.length / 2 - 0.5) * 10; })
+        .text(function (d) { return d; });
+    
+    var tooltip = container.append("div").attr("class", "tooltip").attr("style", "display: none;");
+
+    node
+        .on("mouseover", function (d) {
+            var pos = d3.mouse(container.node());
+            tooltip
+                .html("<label>" + d.data.name + "</label><br/>Number of records: " + d.data.records + "<br/>Total grant value: " + d3.format(",.2f")(d.data.value))
+                .style("left", (pos[0] + 20) + "px").style("top", (pos[1] - 10) + "px").style("display", "inline");
+        })
+        .on("mouseout", function() {
+            tooltip.style("display", "none");
+        })
+        .on("mousemove", function() {
+            var pos = d3.mouse(container.node());
+            tooltip.style("left", (pos[0] + 20) + "px").style("top", (pos[1] - 10) + "px");
+        });
+
+    node.transition().duration(1000).attr("opacity", 1);
+}
+
+function drawLines() {
+    var items = [], valueDict = [], recordDict = [];
+
+    dictPclass.forEach(function(d) {
+        valueDict[d] = [];
+        recordDict[d] = [];
+    });
+
+    chartData.forEach(function(d) {
+        if (valueDict[d.pclass][d.startY] == null) {
+            valueDict[d.pclass][d.startY] = parseFloat(d.total);
+            recordDict[d.pclass][d.startY] = 1;
+
+        } else {
+            valueDict[d.pclass][d.startY] += parseFloat(d.total);
+            recordDict[d.pclass][d.startY]++;
+        }
+    });
+
+    var yearTo = Math.min(startTo, new Date().getFullYear());
+
+    $.each(dictPclass, function(i, d) {
+        var records = recordDict[d];
+        if (records.length == 0) return;
+
+        var item = { id: i, name: (d == null || d == "" ? "[empty]" : d), years: [] };
+
+        for (var j = startFrom; j <= yearTo; j++) {
+            if (valueDict[d][j] != null) {
+                item.years.push({ year: j, value: valueDict[d][j], records: recordDict[d][j] });
+            }
+        }
+
+        items.push(item);
+    });
+
+    $("#divDisplayModes > div").hide();
+    if (items.length == 0) return;
+
+    var margin = { top: 50, right: 50, bottom: 80, left: 60 },
+        width = $container.width() - margin.left - margin.right,
+        height = $container.height() - margin.top - margin.bottom;
+
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+    var x = d3.scaleLinear().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+
+    var line = d3.line().x(function(d) { return x(d.year); }).y(function(d) { return y(d.records); });
+
+    x.domain([
+        d3.min(items, function(d) { return d3.min(d.years, function(d1) { return d1.year; }); }),
+        d3.max(items, function(d) { return d3.max(d.years, function(d1) { return d1.year; }); })
+    ]);
+
+    y.domain([
+        d3.min(items, function(d) { return d3.min(d.years, function(d1) { return d1.records; }); }),
+        d3.max(items, function(d) { return d3.max(d.years, function(d1) { return d1.records; }); })
+    ]);
+
+    var svg = container
+        .append("svg").attr("width", $container.width()).attr("height", $container.height())
+        .append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    svg.append("g").attr("class", "axis axis--x").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x))
+        .append("text").attr("x", margin.left + width / 2 - 40).attr("y", 40).attr("fill", "#000").text("Year of start date");
+
+    svg.append("g").attr("class", "axis axis--y").call(d3.axisLeft(y))
+        .append("text").attr("transform", "rotate(-90)").attr("y", -40).attr("fill", "#000").text("Number of records");
+
+    var series = svg.selectAll(".line").data(items).enter().append("g").attr("class", "line");
+
+    series.append("path").attr("d", function(d) { return line(d.years); }).style("stroke", function(d) { return color(d.name); });
+
+    var vertical = container.append("div").attr("class", "vertical")
+        .style("height", height + "px").style("top", margin.top + "px").style("left", margin.left + "px");
+    
+    var tooltip = container.append("div").attr("class", "tooltip").attr("style", "display: none; width: 200px;");
+
+    container.select("svg")
+        .on("mousemove", function(d) {
+            var posx = d3.mouse(svg.node())[0];
+
+            if (posx < 0 || posx > width) {
+                vertical.style("display", "none");
+                tooltip.style("display", "none");
+                return;
+            }
+
+            vertical.style("display", "inline").style("left", (posx + margin.left) + "px");
+
+            var year = Math.round(x.invert(posx));
+            var html = "<label>" + year + "</label>";
+
+            items.forEach(function(s) {
+                s.years.forEach(function(s1) {
+                    if (s1.year == year) {
+                        html += "<br/><span style='color: " + color(s.name) + "'>" + s.name + ": " + s1.records + "</span>";
+                    }
+                });
+            });
+
+            if (width - posx < 220) {
+                tooltip.html(html).style("left", (posx + margin.left - 230) + "px").style("top", (margin.top) + "px").style("display", "inline");
+            } else {
+                tooltip.html(html).style("left", (posx + margin.left + 10) + "px").style("top", (margin.top) + "px").style("display", "inline");
+            }
+        });
+}
+
+function refreshFilter() {
+    var filterOwner = $("#divFilters ul[data-id='owner']").find("li.active > span").map(function () { return $(this).text(); }).get();
+    var filterPclass = $("#divFilters ul[data-id='pclass']").find("li.active > span").map(function () { return $(this).text(); }).get();
+    var filterOrg_type = $("#divFilters ul[data-id='org_type']").find("li.active > span").map(function () { return $(this).text(); }).get();
+    var filterStatus = $("#divFilters ul[data-id='status']").find("li.active > span").map(function () { return $(this).text(); }).get();
+    var filterGrant = $("#divFilters ul[data-id='grant']").find("li.active > span").map(function () { return $(this).text(); }).get();
+    
+    if (filterPclass.indexOf("[empty]") >= 0) {
+        var pclassIndex = filterPclass.indexOf("[empty]");
+        filterPclass[pclassIndex] = "";
+    }
+
+    chartData = [];
+
+    var ignoreOwner = filterOwner.length == dictOwner.length;
+    var ignorePclass = filterPclass.length == dictPclass.length;
+    var ignoreOrg_type = filterOrg_type.length == dictOrg_type.length;
+    var ignoreStatus = filterStatus.length == dictStatus.length;
+    var ignoreGrant = filterGrant.length == dictGrant.length;
+
+    data.forEach(function (d) {
+        if (!ignoreOwner && filterOwner.indexOf(d.owner) < 0) return;
+        if (!ignorePclass && filterPclass.indexOf(d.pclass) < 0) return;
+        if (!ignoreOrg_type && filterOrg_type.indexOf(d.org_type) < 0) return;
+        if (!ignoreStatus && filterStatus.indexOf(d.status) < 0) return;
+        if (!ignoreGrant && filterGrant.indexOf(d.grant) < 0) return;
+
+        chartData.push(d);
+    });
+    
+    refreshChart();
+}
+
+$(document).ready(function () {
+    $("#divDisplayModes .btn").click(function() {
+        $("#divDisplayModes .btn.active").removeClass("active");
+        $(this).addClass("active");
+
+        refreshChart();
+    });
+
+    $("#divFilters ul").on("click", "a", function () {
+        $(this).closest("ul").find("li.active").removeClass("active");
+        $(this).parent().addClass("active");
+
+        refreshFilter();
+        return false;
+    });
+
+    $("#divFilters ul").on("click", "li", function () {
+        $(this).toggleClass("active");
+        refreshFilter();
+        return false;
+    });
+    
+    $("#divFilters label").on("click", "a", function () {
+        $(this).parent().next().find("li").addClass("active");
+        refreshFilter();
+        return false;
+    });
+
+    d3.csv("data/PhysicsFunding.csv", function (csv) {
+        data = csv;
+        chartData = csv;
+
+        scanValue();
+
+        $("#divDisplayModes .btn").first().click();
+    });
+});
